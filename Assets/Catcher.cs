4ca7b9c97e002817723223874f70e9fb47bfe6c1
Alfie213 <arturbogoratov@gmail.com>
2023-04-02 22:34:47 +0300

    Fixed bug with Health

--- Assets/Catcher.cs
+++ Assets/Catcher.cs
@@ -6,6 +6,7 @@ public class Catcher : MonoBehaviour
 {
     [SerializeField] private Health _health;
     [SerializeField] private FruitChanger _fruitChanger;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Fruit"))
@@ -15,6 +16,8 @@ public class Catcher : MonoBehaviour
                 if (collision.gameObject.GetComponent<SpriteRenderer>().sprite == _fruitChanger.CurrentFruits[i].sprite && !_fruitChanger.Collected[i])
                 {
                     _health.DecreaseHealth();
+                    Destroy(collision.gameObject);
+                    return;
                 }
             }
         }
